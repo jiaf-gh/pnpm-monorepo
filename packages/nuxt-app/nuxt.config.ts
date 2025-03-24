0f134d36935cb6e44ab076eab09e0da2e042021f
jiaf-gh <jiaf.email@proton.me>
2025-03-24 12:23:40 -0300

    feat(nuxt-app): Installed @nuxt/image and added a custom image to use as example.

--- packages/nuxt-app/nuxt.config.ts
+++ packages/nuxt-app/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     port: 4200,
   },
   typescript: { tsConfig: { extends: '../../tsconfig.base.json' } },
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
   eslint: { config: { standalone: false } },
   icon: {
     customCollections: [
@@ -16,5 +16,24 @@ export default defineNuxtConfig({
         dir: './src/assets/icons',
       },
     ],
+  },
+  image: {
+    quality: 80,
+    format: ['webp'],
+    screens: {
+      'sm': 640,
+      'md': 768,
+      'lg': 1024,
+      'xl': 1280,
+      '2xl': 1536,
+    },
+    presets: {
+      avatar: {
+        modifiers: {
+          width: 64,
+          height: 64,
+        },
+      },
+    },
   },
 })
--- packages/nuxt-app/src/app.vue
+++ packages/nuxt-app/src/app.vue
@@ -3,5 +3,22 @@
     <p>Hello Nuxt</p>
     <Icon name="carbon:assembly-cluster" />
     <Icon name="custom:cafe" />
+    <NuxtPicture
+      src="/images/pink-stealth.png"
+      alt="Pink Stealth"
+      width="320px"
+      height="320px"
+      loading="lazy"
+      placeholder
+    />
+    <NuxtPicture
+      src="/images/pink-stealth.png"
+      alt="Pink Stealth"
+      width="64px"
+      height="64px"
+      preset="avatar"
+      loading="lazy"
+      placeholder
+    />
   </div>
 </template>
